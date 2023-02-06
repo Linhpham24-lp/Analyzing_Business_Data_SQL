791b974648b0375bd342d6585771269fc07bd3a0
Linhpham24-lp <124490245+Linhpham24-lp@users.noreply.github.com>
2023-02-06 11:16:38 +0700

    Update 03-Cost and Common Table Expressions (CTEs).sql

--- 03-Cost and Common Table Expressions (CTEs).sql
+++ 03-Cost and Common Table Expressions (CTEs).sql
@@ -25,3 +25,18 @@ FROM meals
 JOIN stock ON meals.meal_id = stock.meal_id
 GROUP BY delivr_month
 ORDER BY delivr_month ASC;
+
+-- Declare a CTE named monthly_cost
+WITH monthly_cost AS (
+  SELECT
+    DATE_TRUNC('month', stocking_date)::DATE AS delivr_month,
+    SUM(meal_cost * stocked_quantity) AS cost
+  FROM meals
+  JOIN stock ON meals.meal_id = stock.meal_id
+  GROUP BY delivr_month)
+
+SELECT
+  -- Calculate the average monthly cost before September
+  avg(cost)
+FROM monthly_cost
+WHERE delivr_month <'2018-09-01'
