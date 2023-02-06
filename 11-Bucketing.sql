5e437d34f6d177927f128acf429bc2dd013ec4f5
Linhpham24-lp <124490245+Linhpham24-lp@users.noreply.github.com>
2023-02-06 18:38:06 +0700

    Create 11-Bucketing.sql

---
+++ 11-Bucketing.sql
@@ -0,0 +1,42 @@
+--Bucketing users by revenue
+WITH user_revenues AS (
+  SELECT
+    -- Select the user IDs and the revenues they generate
+    user_id,
+    SUM(meal_price*order_quantity) AS revenue
+  FROM meals AS m
+  JOIN orders AS o ON m.meal_id = o.meal_id
+  GROUP BY user_id)
+
+SELECT
+  -- Fill in the bucketing conditions
+  CASE
+    WHEN revenue < 150 THEN 'Low-revenue users'
+    WHEN revenue < 300 THEN 'Mid-revenue users'
+    ELSE 'High-revenue users'
+  END AS revenue_group,
+  COUNT(DISTINCT user_id) AS users
+FROM user_revenues
+GROUP BY revenue_group;
+
+
+--Bucketing users by orders
+-- Store each user's count of orders in a CTE named user_orders
+WITH user_orders AS(
+  SELECT
+    user_id,
+    COUNT(DISTINCT order_id) AS orders
+  FROM orders
+  GROUP BY user_id)
+
+SELECT
+  -- Write the conditions for the three buckets
+  CASE
+    WHEN orders < 8 THEN 'Low-orders users'
+    WHEN orders < 15THEN 'Mid-orders users'
+    ELSE 'High-orders users'
+  END AS order_group,
+  -- Count the distinct users in each bucket
+  COUNT(DISTINCT user_id) AS users
+FROM user_orders
+GROUP BY order_group;
